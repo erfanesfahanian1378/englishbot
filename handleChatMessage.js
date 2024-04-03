0c1592df98e9a0883b975699e2dcaecd5fa4d432
ERFAN ESFAHANIAN <erfanesfahanian@ERFANs-MacBook-Pro.local>
2024-04-03 21:36:11 +0200

    code hadler added

--- handleChatMessage.js
+++ handleChatMessage.js
@@ -1,5 +1,6 @@
 // handleChatMessage.js
 const io = require('socket.io-client');
+
 async function handleChatMessage(bot, chatId, messageText) {
     await bot.sendMessage(chatId, "Hi ");
     const socket = io('http://localhost:3002');
@@ -7,7 +8,7 @@ async function handleChatMessage(bot, chatId, messageText) {
     socket.on('connect', () => {
         console.log('Connected to the server');
         // Request a chat upon some user action
-        socket.emit('requestChat', { idchat: chatId , dataSender : "this is the data"});
+        socket.emit('requestChat', {idchat: chatId, dataSender: "this is the data"});
     });
 
 // Listen for a chat partner found event
@@ -15,8 +16,13 @@ async function handleChatMessage(bot, chatId, messageText) {
         console.log(chatId + 'Chat partner found:', data.partnerId);
         // Proceed to establish chat with the partner
     });
+
+    socket.on('chatPartnerDisconnected', (data) => {
+        console.log(chatId + " : " + data);
+        // Proceed to establish chat with the partner
+    });
     socket.on('matchFound', (data) => {
-        console.log(chatId +  'Chat partner found:', data);
+        console.log(chatId + 'Chat partner found:', data);
         // Proceed to establish chat with the partner
     });
 
